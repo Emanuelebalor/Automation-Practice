9fadf3ae92dfd17d5286cd286cca785be7947ee2
HalloIchbinEmanuel <125737443+HalloIchbinEmanuel@users.noreply.github.com>
2023-02-19 12:33:39 +0200

    Update NewTab_Window.py

--- NewTab_Window.py
+++ NewTab_Window.py
@@ -6,6 +6,8 @@ class NewElement(Login):
     def __init__(self):
         super(NewElement, self).__init__()
 
+    # opens a new tab and switches to it, prints a message if it worked by checking if it cna find
+    # an element on the new window
     def new_tab(self):
         self.get("https://qa-practice.netlify.app/tab.html")
         self.find_element(By.ID, "newTabBtn").click()
@@ -16,6 +18,8 @@ class NewElement(Login):
         else:
             print("maybe works")
 
+    # opens a new browser window and switches to it, prints a message if it worked by checking if it cna find
+    # an element on the new window
     def new_window(self):
         self.get("https://qa-practice.netlify.app/window.html")
         self.find_element(By.ID, "newWindowBtn").click()
